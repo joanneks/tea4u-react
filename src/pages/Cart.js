af874b79cf338ef3e9fb7bd030160c8bee2f6730
joanneks <joanne.kan@trentglobal.edu.sg>
2022-12-30 08:50:12 +0000

    stripe troubleshoot success

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -130,10 +130,10 @@ export default function Cart(props) {
     }
     // const checkoutUrl = "https://3000-joanneks-tea4uexpressba-qiw1tvvgol5.ws-us63.gitpod.io/api/checkout"
     // const checkoutUrl = "https://tea4u-express-tgc18.herokuapp.com/api/checkout"
-    const checkoutUrl = "https://tea4ubackend.onrender.com/api/checkout"
+    const checkoutUrl = "https://tea4ubackend.onrender.com/api/checkout";
     
     const checkoutResponse = await axios.post(checkoutUrl, checkoutObject);
-    console.log(checkoutResponse.data)
+    console.log('CHECK',checkoutResponse.data)
     window.location.href = checkoutResponse.data.sessionUrl
   }
 
